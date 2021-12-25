64cbc9f33e1b729c4e4c3557d0b3d2aeaf3f8226
Xi Zhang <xix.zhang@mail.utoronto.ca>
2021-12-24 19:51:48 -0500

    built witht typescript

---
+++ dist/TextRecognizer.tsx
@@ -0,0 +1,70 @@
+import React, {useEffect, useState} from "react";
+import {createScheduler, createWorker} from "tesseract.js";
+
+const scheduler = createScheduler()
+const useTakeWebcamStream = ({video}: { video: HTMLVideoElement | null }) => {
+    useEffect(() => {
+        if (video) {
+            const handleSuccess = (stream: any) => {
+                // Attach the video stream to the video element and autoplay.
+                video.srcObject = stream;
+            };
+            navigator.mediaDevices.getUserMedia({
+                video: true
+            }).then(handleSuccess);
+        }
+    }, [video])
+}
+const useTesseract = () => {
+    useEffect(() => {
+        (async () => {
+            for (let i = 0; i < 4; i++) {
+                const worker = createWorker();
+                await worker.load();
+                await worker.loadLanguage("eng");
+                await worker.initialize("eng");
+                scheduler.addWorker(worker);
+            }
+        })();
+    }, [])
+}
+const doOCR = async (canvas: HTMLCanvasElement): Promise<string> => {
+    const {
+        data: {text}
+    } = await scheduler.addJob("recognize", canvas);
+    return text;
+};
+
+// TODO: use video height and width for snapshot
+function TextRecognizer({onChange}: {onChange: (recognizedText: string) => unknown}) {
+    const [videoRef, setVideoRef] = useState<HTMLVideoElement | null>(null);
+    const [snapShotRef, setSnapShotRef] = useState<HTMLCanvasElement | null>(null);
+    const [results, setResults] = useState<string>("");
+    useTakeWebcamStream({video: videoRef});
+    useTesseract();
+    return (
+        <div className="App">
+            <button id="capture" onClick={() => {
+                if (videoRef && snapShotRef) {
+                    // @ts-ignore
+                    snapShotRef.getContext("2d").drawImage(videoRef, 0, 0, 320, 240)
+                    doOCR(snapShotRef).then((recognizedText) => {
+                        setResults(recognizedText);
+                        onChange(recognizedText);
+                    });
+                }
+            }}>
+                Capture Image
+            </button>
+            <video id="video" controls autoPlay ref={setVideoRef}>
+            </video>
+            <canvas id="snapShot" width="320" height="240" ref={setSnapShotRef}>
+            </canvas>
+            <div id="results">
+                {results}
+            </div>
+        </div>
+    );
+}
+
+export default TextRecognizer;
---
+++ dist/index.ts
@@ -0,0 +1,5 @@
+import TextRecognizer from "./TextRecognizer";
+
+export {
+    TextRecognizer
+}
